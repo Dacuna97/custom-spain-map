8f5a6b218f846c4957927d33a34a5a20e61ec4d7
Dacuna97 <diegoacunaberger@gmail.com>
2021-07-06 16:50:49 +0200

    pot

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import { VectorMap } from '@south-paw/react-vector-maps';
-import { google } from 'googleapis';
-import keys from '../keys.json';
 import spainProvinces from '../public/spain-provinces';
 
 import styles from '../styles/Home.module.css';
@@ -13,80 +11,14 @@ export default function Home({ values }) {
   const onClick = ({ target }) => {
     const id = target.attributes.id.value;
     const name = target.attributes.name.value;
-
+    alert(name);
     setSelected(name);
     setSelectedId(id);
   };
 
-  const headings = values && values.length > 0 ? values[0] : [];
-  const helps = values && values.length > 1 ? values.filter((value) => value[3] === selected) : [];
   return (
     <div className={styles.container}>
-      {headings && headings.length > 0 && helps && helps.length > 0 && (
-        <div className='province-info-wrapper'>
-          <div className='province-info'>
-            <div className='name-wrapper'>
-              <div className='heading name-heading'>{headings[0]}</div>
-              {helps.map((help) => (
-                <div className='name'>{help[0]}</div>
-              ))}
-            </div>
-            <div className='typology-wrapper'>
-              <div className='heading typology-heading'>{headings[1]}</div>
-              {helps.map((help) => (
-                <div className='typology' style={{ backgroundColor: help[1] }}></div>
-              ))}
-            </div>
-            <div className='status-wrapper'>
-              <div className='heading status-heading'>{headings[2]}</div>
-              {helps.map((help) => (
-                <div className='status' style={{ backgroundColor: help[2] }}></div>
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
       <VectorMap {...spainProvinces} layerProps={{ onClick }} checkedLayers={[selectedId]} />
     </div>
   );
-}
-
-async function gsrun(cl) {
-  try {
-    const gsapi = google.sheets({ version: 'v4', auth: cl });
-
-    const opt = {
-      spreadsheetId: '18iw-XEqUMUMmlyrp-5PHgQAIUjzyfHaX015GHF_fvtI',
-      range: 'Data'
-    };
-
-    let response = await gsapi.spreadsheets.values.get(opt);
-    let values = [];
-    if (response && response.data && response.data.values) {
-      values = response.data.values;
-    }
-    console.log('values', values);
-    return values;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-export async function getServerSideProps(ctx) {
-  try {
-    const client = new google.auth.JWT(keys.client_email, null, keys.private_key, [
-      'https://www.googleapis.com/auth/spreadsheets.readonly'
-    ]);
-
-    const tokens = await client.authorize();
-    console.log('Connected');
-    const values = await gsrun(client);
-    return {
-      props: {
-        values
-      }
-    };
-  } catch (err) {
-    console.log(err);
-  }
 }
